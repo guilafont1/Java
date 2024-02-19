caaa4bcd27d21e22df8541fed4a23b745999d80e
outski <outski.abdelhakim@gmail.com>
2024-02-19 20:58:29 +0100

    update database conf

--- gestion-des-comptes/src/main/java/com/gestion/compte/gestiondescomptes/repository/CompteRepository.java
+++ gestion-des-comptes/src/main/java/com/gestion/compte/gestiondescomptes/repository/CompteRepository.java
@@ -2,7 +2,9 @@ package com.gestion.compte.gestiondescomptes.repository;
 
 import com.gestion.compte.gestiondescomptes.model.compte;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompteRepository extends CrudRepository<compte,Long> {
 
 }
